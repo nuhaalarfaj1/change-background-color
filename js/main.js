ffb2595a8074e28d930b1175dc864f9a98e1c642
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-04-17 15:00:54 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+
+var input = document.getElementById("colorsname");
+
+function changecolor(){
+var usercolor = document.getElementById("color").value;
+    document.body.style.backgroundColor = usercolor;
+}
+
+
